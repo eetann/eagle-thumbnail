184a97f210d4f2a2f731bb38d57c7615c24aa1fd
eetann <hideharu092@gmail.com>
2024-06-10 16:48:04 +0900

    refactor: 関数を別ファイルに移動

--- src/entrypoints/content/waitForElement.ts
+++
@@ -1,5 +0,0 @@
-export async function waitForElement(
-	anchor: string,
-): Promise<HTMLElement | null> {
-	return new Promise((resolve) => {});
-}
---
+++ src/entrypoints/content/waitForElm.ts
@@ -0,0 +1,47 @@
+export async function waitForElm(
+	selector: string,
+): Promise<Element | undefined> {
+	// NOTE: https://phuoc.ng/collection/html-dom/waiting-for-an-element-to-become-available/
+	// https://macarthur.me/posts/use-mutation-observer-to-handle-nodes-that-dont-exist-yet/
+	// setIntervalだと要素が多いと大変っぽい
+	return new Promise((resolve) => {
+		const elm = document.querySelector(selector);
+		if (elm) {
+			resolve(elm);
+			return;
+		}
+		const observer = new MutationObserver((mutations) => {
+			for (const mutation of mutations) {
+				if (mutation.addedNodes.length === 0) {
+					continue;
+				}
+				for (const node of mutation.addedNodes) {
+					if (!(node instanceof HTMLElement)) continue;
+					if (node.matches(selector)) {
+						observer.disconnect();
+						clearTimeout(timeout);
+						resolve(node);
+						return;
+					}
+				}
+			}
+			// const elm = document.querySelector(selector);
+			// if (elm) {
+			// 	observer.disconnect();
+			//      clearTimeout(timeout);
+			// 	resolve(elm);
+			// 	return;
+			// }
+		});
+		const timeout = setTimeout(() => {
+			observer.disconnect();
+			clearTimeout(timeout);
+			resolve(undefined);
+			return;
+		}, 15000);
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true,
+		});
+	});
+}
