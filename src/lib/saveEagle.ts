20303308ec51bcfe3da7be4c9868468832ee57ce
eetann <hideharu092@gmail.com>
2024-07-06 18:09:19 +0900

    feat: Eagleのポップアップを開けるように変更

--- src/lib/saveEagle.ts
+++ src/lib/saveEagle.ts
@@ -1,5 +1,6 @@
 import { storage } from "wxt/storage";
 
+// TODO: 必要なければ消す
 const apiTokenStorage = storage.defineItem<string>("local:apiTokenStorage", {
 	defaultValue: "",
 });
@@ -12,26 +13,23 @@ export async function setApiToken(token: string) {
 	return await apiTokenStorage.setValue(token);
 }
 
-export async function save(name: string, url: string, website: string) {
-	const apiToken = await getApiToken();
-	if (apiToken === "") {
-		throw new Error("set API Token");
-	}
+export async function save(name: string, srcURL: string, websiteURL: string) {
 	const data = {
-		token: apiToken,
-		url,
-		name,
-		website,
-		tags: [],
+		type: "image",
+		title: name,
+		src: srcURL,
+		url: websiteURL,
 	};
 
 	const requestOptions: RequestInit = {
 		method: "POST",
-		body: JSON.stringify(data),
-		redirect: "follow",
+		body: new URLSearchParams(data),
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
 	};
 
-	const apiURL = "http://localhost:41595/api/item/addFromURL";
+	const apiURL = "http://localhost:41593";
 	fetch(apiURL, requestOptions)
 		.then((response) => response.json())
 		.then((result) => console.log(result))
