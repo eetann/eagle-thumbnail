38f49c1055cce98faf1e978cc7d9c4dcc1cd5329
eetann <hideharu092@gmail.com>
2024-07-10 19:13:26 +0900

    refactor: 使っていないので一旦削除

--- src/entrypoints/popup/main.ts
+++
@@ -1,9 +0,0 @@
-import "@/assets/app.css";
-import App from "./App.svelte";
-
-const app = new App({
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	target: document.getElementById("app")!,
-});
-
-export default app;
